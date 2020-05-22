99c4e6bbf103db13481ab915f2546879537d9e3d
Patrick Yoho <patrick@innovateedunyc.org>
2020-05-21 22:34:06 -0500

    Removes download hack for headless since Chrome 83+ should fully support headless downloads

--- ducttape/utils.py
+++ ducttape/utils.py
@@ -209,27 +209,7 @@ class DriverBuilder:
 
         driver = Chrome(options=chrome_options)
 
-        if headless:
-            self._enable_download_in_headless_chrome(driver, download_location)
-
         return driver
-
-    def _enable_download_in_headless_chrome(self, driver, download_dir):
-        """
-        there is currently a "feature" in chrome where
-        headless does not allow file download: https://bugs.chromium.org/p/chromium/issues/detail?id=696481
-        This method is a hacky work-around until the official chromedriver support for this.
-        Requires chrome version 62.0.3196.0 or above.
-        """
-
-        # add missing support for chrome "send_command"  to selenium webdriver
-        driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-
-        params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
-        command_result = driver.execute("send_command", params)
-        print("response from browser:")
-        for key in command_result:
-            print("result:" + key + ":" + str(command_result[key]))
 
 
 class LoggingMixin(object):
