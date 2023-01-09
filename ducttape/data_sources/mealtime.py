1f410f031a4e41953d875f15bc6e9115927522f0
howard_shen <hshen@summitps.org>
2023-01-09 11:51:35 -0500

    Forgot an import in the mealtime data source

--- ducttape/data_sources/mealtime.py
+++ ducttape/data_sources/mealtime.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
